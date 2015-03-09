ccddcd1e5cfcfef14eb70054fd61f85c4407c238
iavidov <iavidov@backstopsolutions.com>
2015-03-09 13:24:56 -0500

    # changed hash to new ruby style in Guardfile # added new options to be stet in guardfile.

--- lib/guard/java.rb
+++ lib/guard/java.rb
@@ -1,32 +1,32 @@
 require 'guard/compat/plugin'
 
 module Guard
-  class Java < Plugin 
+  class Java < Plugin
 
-   def initialize(options={})
+    def initialize(options={})
       super
       @options = {
-        :all_after_pass          => true,
-        :all_on_start            => true,
-        :focused_after_compile   => true,
-        :test_runner_class       => 'org.junit.runner.JUnitCore',
-        :project_name            => 'Java Project',
-        :focused_cli             => nil,
-        :all_cli                 => nil,
-        :classpath               => '.'
+          all_after_pass:               true,
+          all_on_start:                 true,
+          source_directory:               '',
+          focused_after_compile:        true,
+          test_runner_class:            'org.junit.runner.JUnitCore',
+          project_name:                 'Java Project',
+          focused_compile_cli_command:  nil,
+          all_compile_cli_command:      nil,
+          classpath:                    '.'
       }.merge(options)
     end
 
     def start
       UI.info 'Guard::Java is running'
-      raise ArgumentError, ":focused_cli and :all_cli options must be set" if @options[:focused_cli].nil? || @options[:all_cli].nil?
-
+      raise ArgumentError, ':focused_cli and :all_cli options must be set' if @options[:focused_compile_cli_command].nil? || @options[:all_compile_cli_command].nil?
       run_all if @options[:all_on_start]
     end
 
     def run_all
       project_name = @options[:project_name]
-      notify project_name, "Build and run all", :pending
+      notify project_name, 'Build and run all', :pending
       result = do_shell(all_command)
       result_description = "#{project_name} build results"
       notify result_description, "Build #{result.to_s.capitalize}", result # Notify of success or failure
@@ -37,11 +37,8 @@ module Guard
       klass = classes[0]
 
       result = compile(project_name, klass)
-
       result = run_focused_tests(project_name, klass) if result != :failed && @options[:focused_after_compile]
-
       result_description = "#{project_name}: test run for #{klass}"
-
       notify result_description, "Build #{result.to_s.capitalize}", result # Notify of success or failure
 
       if result == :success && @options[:all_after_pass]
@@ -52,19 +49,32 @@ module Guard
 
     def compile(project_name, klass)
       notify "Compiling because of #{klass} change", "#{project_name} file change detected", :pending  # notify any interested listeners
-      do_shell(focused_command)
+      klass_path = klass.dup
+      klass_path.gsub! '.', '/'
+      UI.info "running #{focused_compile_command(klass)}"
+      do_shell(focused_compile_command(klass_path))
+    end
+
+    def focused_compile_command(klass)
+      "#{@options[:focused_compile_cli_command]}#{@options[:source_directory]}#{klass}.java"
     end
 
     def run_focused_tests(project_name, klass)
+      notify "Running focused tests because of #{klass} change", "#{project_name} file change detected", :pending  # notify any interested listeners
+      unless klass.end_with? 'Test'
+        klass << 'Test'
+      end
       run_test_class(klass)
     end
 
-    def focused_command
-      @options[:focused_cli]
+    def run_test_class(klass)
+      test_command = "java -cp #{options[:classpath]} #{options[:test_runner_class]} #{klass}"
+      UI.info "running test command #{test_command}"
+      do_shell test_command
     end
 
     def all_command
-      @options[:all_cli]
+      @options[:all_compile_cli_command]
     end
 
     def notify(msg, title='', image=nil)
@@ -80,12 +90,6 @@ module Guard
 
       code = $?  # Get the status code of the last-finished process
       (code == 0) ? :success : :failed
-    end
-
-    def run_test_class(klass)
-      test_command = "java -cp #{options[:classpath]} #{options[:test_runner_class]} #{klass}"
-      puts test_command
-      do_shell test_command
     end
 
   end
